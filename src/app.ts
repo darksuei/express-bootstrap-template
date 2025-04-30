a5dc5ab8ee13db32caa0ef68d1d676afe839a943
Folarin Raphael <chrisraphael576@gmail.com>
2025-04-30 21:40:21 +0100

    feat: add compression & fix swagger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import compression from "compression";
 import setupRoutes from "./setup/setupRoutes";
 import setupBeforeMiddlewares from "./setup/setupBeforeMiddlewares";
 import setupAfterMiddlewares from "./setup/setupAfterMiddlewares";
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(compression());
+app.use(express.static("public"));
 
 setupBeforeMiddlewares(app);
 setupRoutes(app);
